17fb5987a2b9c325a014f1123293f06069a3069e
Ваня Янголь <vanyangol@MacBook-Air-Vana.local>
2025-11-22 00:54:42 +0200

    Concept of functions in header files

---
+++ board.h
@@ -0,0 +1,11 @@
+#ifndef BOARD
+#define BOARD
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+#include <string.h>
+
+void board_init(); //ініціалізує структуру судоку, очищає всі минулі масиви
+
+#endif // BOARD
