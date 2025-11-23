8738a1adc0b7e6c8caae94774519f29b955304fb
Dan Sokolovsky <dv.sokolovskyi@ukma.edu.ua>
2025-11-23 22:59:51 +0200

    headerfiles after a mistake

---
+++ lib/include/user.h
@@ -0,0 +1,14 @@
+#ifndef USER_H
+#define USER_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdbool.h>
+#include <string.h>
+#include "board.h"
+
+void board_print(Sudoku *s);    /*
+відображає ігрове поле у консолі
+*/
+
+#endif // USER
